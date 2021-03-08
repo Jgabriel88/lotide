f59a9fe27ca22c24c3a40cd72dc5d49d1336cf04
Gabriel Silva <j.gabriel.888@gmail.com>
2021-03-08 14:37:34 +0000

    refactor tail with module

---
+++ test/tailTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail')
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
