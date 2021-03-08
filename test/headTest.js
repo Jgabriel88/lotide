41d1589bed6a7b93a922189556e52f10d196603b
Gabriel Silva <j.gabriel.888@gmail.com>
2021-03-08 18:46:37 +0000

    MochaChai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,22 @@
+const assert = require('chai').assert;
+const head = require('../head');
 const assertEqual = require('../assertEqual');
-const head = require('../head')
 
-assertEqual(head([5, 6, 7]), 6);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 4);
-assertEqual(head([1]), 1);
-assertEqual(head([]), 4);
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it('returns "hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello');
+  });
+  it('returns I for ["I", "am", "a", "student", "at", "LHL"]', () => {
+    assert.strictEqual(head(["I", "am", "a", "student", "at", "LHL"]), "I");
+  });
+
+
+});
